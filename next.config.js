15a461919fc9e28bb483001b9570e57ea050bfcb
xphnx-rises <sh.xphnx@gmail.com>
2022-08-03 07:32:23 +0300

    build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+  basePath: '/nextjs-gh-pages',
+  assetPrefix: '/nextjs-gh-pages',
 }
 
 module.exports = nextConfig
